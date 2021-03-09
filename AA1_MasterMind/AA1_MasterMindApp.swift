4d461a2586c5c0534ae96b8aad0a2472f1d31061
AresFernandez <ares.fernandez@enti.cat>
2021-03-09 18:55:05 +0100

    Initial Commit

---
+++ AA1_MasterMind/AA1_MasterMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  AA1_MasterMindApp.swift
+//  AA1_MasterMind
+//
+//  Created by Alumne on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AA1_MasterMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
